6954f73644eb64e841dffd20b41d0c3d0891daa6
Misha Bondar <mishabondar44@gmail.com>
2024-06-08 18:22:43 +0200

    NEW Without cyfry

--- update_sba.py
+++ update_sba.py
@@ -2,34 +2,35 @@ import sys
 lista=[]
 input_params = sys.argv
 lista_jakie_usunac=[]
-nowy = open("new_sba-tests.yaml", "w")
-
-def usuwanie(lista):
-    liczby = ['1', '2', '3']
-    for i in lista:
-        if i not in liczby:
-            indeks=lista.index(i)
-            lista.pop(indeks)
-            usuwanie(lista)
-    return(lista)
+origin = open("sba-tests_origin.yaml", "w")
+nowy=open("new_sba-tests.yaml", "w")
 
 
+test=[]
 for i in input_params[1][16:]:
-    lista.append(i)
-for i in usuwanie(lista):
-    lista_jakie_usunac.append(i)
+
+    if i == ',':
+        slowo =''.join(test)
+        lista.append(slowo)
+        test[:]=[]
+    else:
+        test.append(i)
+if test:
+    slowo = ''.join(test)
+    lista.append(slowo)
+
+with open("sba-tests.yaml") as plik:
+    linie = plik.readlines()
+    for linia in linie:
+        origin.write(linia)
 with open("sba-tests.yaml") as plik:
     linie = plik.readlines()
     nowy.write(linie[0])
     nowy.write(linie[1])
     for linia in linie[2:]:
-        logic = True
-        for i in linia:
-            if i in lista_jakie_usunac:
-                logic = False
-        if logic == True:
-            nowy.write(linia)
+        slowo = linia[7:].strip()
+        if slowo in lista:
+            nowy.write("#"+ linia)
         else:
-            nowy.write("#"+linia)
-
+            nowy.write(linia)
 
